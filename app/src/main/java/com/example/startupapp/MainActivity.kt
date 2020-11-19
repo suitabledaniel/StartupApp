601eedf4159475d9d62d21db04c53e0cf1ad0cfe
daniel <daniel.kim.suitable@gmail.com>
2020-11-19 22:58:56 +0900

    create Startup App

---
+++ app/src/main/java/com/example/startupapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.startupapp
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+class MainActivity : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val rootView: View = inflater.inflate(R.layout.fragment_first, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+        return rootView
+    }
+
+
+    override fun onMapReady(p0: GoogleMap?) {
+        TODO("Not yet implemented")
+    }
+}
