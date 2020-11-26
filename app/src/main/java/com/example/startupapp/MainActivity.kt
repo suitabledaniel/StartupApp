00784889af2103540cc2af7cf641fe898108ee53
suitabledaniel <daniel.kim.suitable@gmail.com>
2020-11-26 16:12:25 +0900

    test new

--- app/src/main/java/com/example/startupapp/MainActivity.kt
+++ app/src/main/java/com/example/startupapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.startupapp
 
 import android.content.Context
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.AttributeSet
 import android.view.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -13,29 +14,28 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions;
 
-class MainActivity : Fragment(), OnMapReadyCallback {
-
-    private lateinit var mMap: GoogleMap
-
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        val rootView: View = inflater.inflate(R.layout.fragment_first, container, false)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-        return rootView
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mMap: GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val SEOUL = LatLng(37.56,126.97)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(SEOUL)
+        markerOptions.title("SEOUL")
+        markerOptions.snippet("Center of KOREA")
+        mMap!!.addMarker(markerOptions)
 
-    override fun onMapReady(googleMap : GoogleMap?) {
-        if (googleMap != null) {
-            mMap = googleMap
-        }
-        val marker = LatLng(35.241615, 128.695587)
-        mMap.addMarker(MarkerOptions().position(marker).title("Marker KIM"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10f))
     }
+
 }
