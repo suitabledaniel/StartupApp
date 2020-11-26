3e1c939189b449de1086daffb2789cc04efc42fc
daniel <daniel.kim.suitable@gmail.com>
2020-11-26 16:21:27 +0900

    test 2 commit

--- app/src/main/java/com/example/startupapp/MainActivity.kt
+++ app/src/main/java/com/example/startupapp/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.example.startupapp
 
-import android.content.Context
+
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.AttributeSet
-import android.view.*
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,7 +10,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions;
-
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private var mMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +21,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val SEOUL = LatLng(37.56,126.97)
+        val SEOUL = LatLng(128.1076213 , 35.1799817)
         val markerOptions = MarkerOptions()
         markerOptions.position(SEOUL)
-        markerOptions.title("SEOUL")
-        markerOptions.snippet("Center of KOREA")
+        markerOptions.title("JINJU")
+        markerOptions.snippet("GYEUNANM")
         mMap!!.addMarker(markerOptions)
 
         mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10f))
