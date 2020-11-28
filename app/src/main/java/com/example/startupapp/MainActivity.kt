c102943038347b249a9bc3770b7c204bad1cf634
daniel <daniel.kim.suitable@gmail.com>
2020-11-28 12:51:03 +0900

    add editText & searchBtn

--- app/src/main/java/com/example/startupapp/MainActivity.kt
+++ app/src/main/java/com/example/startupapp/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.startupapp
 
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,13 +14,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions;
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListener {
     private var mMap: GoogleMap? = null
+    private var searchBtn: Button? = null
+    private var editTxt: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment?
         mapFragment!!.getMapAsync(this)
+        editTxt = findViewById(R.id.search_edit_txt)
+        searchBtn = findViewById(R.id.search_btn)
+        searchBtn!!.setOnClickListener {
+            Log.d("EditText",editTxt.toString())
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -29,6 +41,10 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap!!.addMarker(markerOptions)
 
         mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(JINJU, 10f))
+    }
+
+    override fun onClick(v: View?) {
+
     }
 
 }
