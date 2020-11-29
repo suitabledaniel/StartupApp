5ae5edf37e1b56e870b3c72ed9941620abe1b8b7
daniel <daniel.kim.suitable@gmail.com>
2020-11-29 12:58:09 +0900

    new auto text

--- app/src/main/java/com/example/startupapp/MainActivity.kt
+++ app/src/main/java/com/example/startupapp/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.startupapp
 
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Selection.setSelection
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +21,7 @@ import com.google.android.gms.tasks.Tasks
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.AutocompleteSessionToken
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.PlacesClient
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
@@ -27,58 +32,39 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
     private var searchBtn: Button? = null
     private var editTxt: EditText? = null
     private val apiKey = "AIzaSyCPD3p0R_wNvtK_IktkKZCjSsT99UUES8A"
+    private lateinit var mPlacesClient: PlacesClient
+    private var placeAdapter:PlaceArrayAdapter? = null
+
     private val LATLNG = mapOf("Lat" to 35.1799817, "Lng" to 128.1076213)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment?
-        mapFragment!!.getMapAsync(this)
-        // Initialize the SDK
-        Places.initialize(applicationContext, apiKey)
-        // Create a new PlacesClient instance
-        val placesClient = Places.createClient(this)
-        //var placeFiled = listOf<Place.Field>(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG, Place.Field.ADDRESS);
 
-
-        editTxt = findViewById(R.id.search_edit_txt)
-        searchBtn = findViewById(R.id.search_btn)
-        searchBtn!!.setOnClickListener {
+        //editTxt = findViewById(R.id.search_edit_txt)
+        searchBtn = findViewById(R.id.search_btn) as Button
+        searchBtn?.setOnClickListener {
             Log.d("EditText", editTxt!!.text.toString())
-            val token = AutocompleteSessionToken.newInstance()
-            val request = FindAutocompletePredictionsRequest.builder()
-                    .setSessionToken(token)
-                    .setQuery(editTxt!!.text.toString())
-                    .build()
-
-            val autocompletePredictions = placesClient.findAutocompletePredictions(request)
-
-            // This method should have been called off the main UI thread. Block and wait for at most
-            // 60s for a result from the API.
-
-            // This method should have been called off the main UI thread. Block and wait for at most
-            // 60s for a result from the API.
-            try {
-                Tasks.await(autocompletePredictions, 60, TimeUnit.SECONDS)
-            } catch (e: ExecutionException) {
-                e.printStackTrace()
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
-            } catch (e: TimeoutException) {
-                e.printStackTrace()
-            }
-
-            /*return*/ if (autocompletePredictions.isSuccessful) {
-                val findAutocompletePredictionsResponse = autocompletePredictions.result
-                if (findAutocompletePredictionsResponse != null) for (prediction in findAutocompletePredictionsResponse.autocompletePredictions) {
-                    Log.i(TAG, prediction.placeId)
-                    //resultList.add(PlaceAutocomplete(prediction.placeId, prediction.getPrimaryText(STYLE_NORMAL).toString(), prediction.getFullText(STYLE_BOLD).toString()))
-                }
-                //resultList
-            } else {
-                //resultList
-            }
-
         }
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        Places.initialize(this, apiKey)
+        mPlacesClient = Places.createClient(this)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+        placeAdapter = PlaceArrayAdapter(this, R.layout.layout_item_places, mPlacesClient)
+        var autoCompleteEditText = findViewById(R.id.autoCompleteEditText) as AutoCompleteTextView
+        autoCompleteEditText.setAdapter(placeAdapter)
+
+        autoCompleteEditText.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
+            val place = parent.getItemAtPosition(position) as PlaceDataModel
+            autoCompleteEditText.apply {
+                setText(place.fullText)
+                setSelection(autoCompleteEditText.length())
+            }
+        }
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
---
+++ app/src/main/java/com/example/startupapp/PlaceDataModel.kt
@@ -0,0 +1,8 @@
+package com.example.startupapp
+/**
+ * This class is used to save place data
+ * @property placeId String?
+ * @property fullText String
+ * @constructor
+ */
+data class PlaceDataModel(var placeId: String?, var fullText: String = "")
