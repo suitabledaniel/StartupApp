1e48678cfc590be9473daa3cde5a4b666c133e96
suitabledaniel <daniel.kim.suitable@gmail.com>
2021-11-06 17:22:17 +0900

    backup for Test

--- app/src/main/java/com/example/startupapp/FirstFragment.kt
+++ app/src/main/java/com/example/startupapp/FirstFragment.kt
@@ -1,31 +1,130 @@
 package com.example.startupapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.FetchPlaceResponse
+import com.google.android.libraries.places.api.net.PlacesClient
+import java.lang.Exception
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), OnMapReadyCallback, View.OnClickListener {
+    private lateinit var mPlacesClient: PlacesClient
+    private val apiKey = "AIzaSyCPD3p0R_wNvtK_IktkKZCjSsT99UUES8A"
+    private var placeAdapter:PlaceArrayAdapter? = null
+    private var mMap: GoogleMap? = null
+    private var searchBtn: Button? = null
+    private var mPosition : Int? = null
+    private val LATLNG = mapOf("Lat" to 35.1799817, "Lng" to 128.1076213)
+    private var mFragmentView : View? = null;
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        /*Create PlaceClient for init*/
+        Places.initialize(container!!.context, apiKey)
+        mFragmentView = inflater.inflate(R.layout.fragment_first, container, false)
+        mPlacesClient = Places.createClient(container!!.context)
+        /*Create mapFragement for init */
+        val mapFragment = requireFragmentManager().findFragmentById(R.id.mapview) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+        placeAdapter = PlaceArrayAdapter(container!!.context, R.layout.layout_item_places, mPlacesClient)
+        //var autoCompleteEditText = requireView().findViewById(R.id.autoCompleteEditText) as AutoCompleteTextView
+        var autoCompleteEditText = mFragmentView!!.findViewById(R.id.autoCompleteEditText) as AutoCompleteTextView
+
+        autoCompleteEditText.setAdapter(placeAdapter)
+        searchBtn = mFragmentView!!.findViewById(R.id.search_btn) as Button
+        searchBtn?.setOnClickListener {
+            Log.d("TEST::SearchBtnClick", autoCompleteEditText?.text.toString())
+        }
+
+        autoCompleteEditText.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
+
+            val place = parent.getItemAtPosition(position) as PlaceDataModel
+            autoCompleteEditText.apply {
+                Log.d("TEST::autoComplete", place.placeId.toString())
+                Log.d("TEST::autoComplete", position.toString())
+
+                val placeFields: List<Place.Field> = listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.OPENING_HOURS, Place.Field.WEBSITE_URI, Place.Field.RATING, Place.Field.LAT_LNG)
+                val request = FetchPlaceRequest.newInstance(place.placeId.toString(), placeFields)
+                mPlacesClient.fetchPlace(request)
+                        .addOnSuccessListener { response: FetchPlaceResponse ->
+                            val place = response.place
+                            Log.i("TEST::PlacesClient", "Place Found : ${place.name}")
+                            Log.i("TEST::PlacesClient", "Place Found : ${place.address}")
+                            Log.i("TEST::PlacesClient", "Place Found : ${place.websiteUri}")
+                            Log.i("TEST::PlacesClient", "Place Found : ${place.latLng}")
+                            Log.i("TEST::PlacesClient", "Place Found : ${place.rating}")
+                            Log.i("TEST::PlacesClient", "Place Found : ${place.isOpen}")
+                            setMarkerDetailPlace(place);
+                        }.addOnFailureListener{exception: Exception ->
+                            if (exception is ApiException) {
+                                Log.e("TEST::::PlacesClient", "Place not found: ${exception.message}")
+                                val statusCode = exception.statusCode
+                                TODO("Handle error with given status code")
+                            }
+                        }
+                mPosition = position
+                setText(place.fullText)
+                setSelection(autoCompleteEditText.length())
+            }
+        }
+        return mFragmentView;
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
+    }
+    private fun setMarkerDetailPlace(place : Place){
+        if(place.latLng != null){
+            val newPlace = LatLng(place.latLng!!.latitude, place.latLng!!.longitude)
+            val markerOptions = MarkerOptions()
+            markerOptions.position(newPlace)
+            markerOptions.title(place.name.toString())
+            if (place.address != null) markerOptions.snippet(place.address)
+            mMap!!.addMarker(markerOptions)
+            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(newPlace, 10f))
+        }else {
+            Log.e("TEST", "Place not found!")
         }
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val JINJU = LatLng((LATLNG["Lat"] ?: error("didn't Double Type")).toDouble(), (LATLNG["Lng"]
+                ?: error("didn't Double Type")).toDouble())
+        val markerOptions = MarkerOptions()
+        markerOptions.position(JINJU)
+        markerOptions.title("JINJU")
+        markerOptions.snippet("GYEUNANM")
+        mMap!!.addMarker(markerOptions)
+
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(JINJU, 10f))
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/startupapp/MainActivity.kt
+++ app/src/main/java/com/example/startupapp/MainActivity.kt
@@ -23,34 +23,17 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.customnavigationdrawerexample.DemoFragment
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.net.FetchPlaceRequest
-import com.google.android.libraries.places.api.net.FetchPlaceResponse
-import com.google.android.libraries.places.api.net.PlacesClient
+
 import java.lang.Exception
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListener {
-    private var mMap: GoogleMap? = null
-    private var searchBtn: Button? = null
-    private var placeAdapter:PlaceArrayAdapter? = null
-
-    private lateinit var mPlacesClient: PlacesClient
-    private val apiKey = "AIzaSyCPD3p0R_wNvtK_IktkKZCjSsT99UUES8A"
+class MainActivity : AppCompatActivity() {
 
 
-    private var mPosition : Int? = null
     lateinit var drawerLayout: DrawerLayout
     private lateinit var adapter: NavigationRVAdapter
     private lateinit var navigation_rv: RecyclerView
+    private lateinit var navigation_layout: LinearLayout
     private var items = arrayListOf(
         NavigationItemModel(R.drawable.ic_home, "Home"),
         NavigationItemModel(R.drawable.ic_music, "Music"),
@@ -61,30 +44,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
         NavigationItemModel(R.drawable.ic_social, "Like us on facebook")
     )
 
-    private val LATLNG = mapOf("Lat" to 35.1799817, "Lng" to 128.1076213)
 
 
     @SuppressLint("LongLogTag")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Places.initialize(this, apiKey)
 
-        /*Create PlaceClient for init*/
-        mPlacesClient = Places.createClient(this)
-        /*Create mapFragement for init */
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
-        placeAdapter = PlaceArrayAdapter(this, R.layout.layout_item_places, mPlacesClient)
-        var autoCompleteEditText = findViewById(R.id.autoCompleteEditText) as AutoCompleteTextView
-        autoCompleteEditText.setAdapter(placeAdapter)
 
         /*Create Navigation View */
         drawerLayout = findViewById(R.id.drawer_layout)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        navigation_rv.findViewById<RecyclerView>(R.id.navigation_rv).layoutManager = LinearLayoutManager(this)
-        navigation_rv.findViewById<RecyclerView>(R.id.navigation_rv).setHasFixedSize(true)
+        navigation_rv = findViewById(R.id.navigation_rv) as RecyclerView
+        navigation_rv.layoutManager = LinearLayoutManager(this)
+        navigation_rv.setHasFixedSize(true)
 
         // Add Item Touch Listener
         navigation_rv.addOnItemTouchListener(RecyclerTouchListener(this, object : ClickListener {
@@ -94,36 +68,36 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
                         // # Home Fragment
                         val homeFragment = DemoFragment()
                         supportFragmentManager.beginTransaction()
-                            .replace(R.id.activity_main_content_id, homeFragment).commit()
+                           .replace(R.id.main_activity_content_id, homeFragment).commit()
                     }
                     1 -> {
-                        // # Music Fragment
-                        val musicFragment = DemoFragment()
+                        // # Camera Fragment
+                        val firstFragment = FirstFragment()
                         supportFragmentManager.beginTransaction()
-                            .replace(R.id.activity_main_content_id, musicFragment).commit()
+                               .replace(R.id.main_activity_content_id, firstFragment).commit()
                     }
-                    2 -> {
+                    2 ->{
                         // # Movies Fragment
                         val moviesFragment = DemoFragment()
                         supportFragmentManager.beginTransaction()
-                            .replace(R.id.activity_main_content_id, moviesFragment).commit()
+                          .replace(R.id.main_activity_content_id, moviesFragment).commit()
                     }
                     3 -> {
-                        // # Books Fragment
+                      // # Books Fragment
                         val booksFragment = DemoFragment()
                         supportFragmentManager.beginTransaction()
-                            .replace(R.id.activity_main_content_id, booksFragment).commit()
+                            .replace(R.id.main_activity_content_id, booksFragment).commit()
                     }
                     4 -> {
                         // # Profile Activity
                         //val intent = Intent(this@MainActivity, DemoActivity::class.java)
-                       // startActivity(intent)
+                        //startActivity(intent)
                     }
                     5 -> {
                         // # Settings Fragment
                         val settingsFragment = DemoFragment()
                         supportFragmentManager.beginTransaction()
-                            .replace(R.id.activity_main_content_id, settingsFragment).commit()
+                            .replace(R.id.main_activity_content_id, settingsFragment).commit()
                     }
                     6 -> {
                         // # Open URL in browser
@@ -174,51 +148,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
 
         val homeFragment = DemoFragment()
         supportFragmentManager.beginTransaction()
-            .replace(R.id.activity_main_content_id, homeFragment).commit()
+            .replace(R.id.main_activity_content_id, homeFragment).commit()
 
         // Set Header Image
         findViewById<ImageView>(R.id.navigation_header_img).setImageResource(R.drawable.logo)
 
         // Set background of Drawer
-        findViewById<ImageView>(R.id.navigation_layout).setBackgroundColor(ContextCompat.getColor(this, R.color.design_default_color_primary))
+        navigation_layout = findViewById(R.id.navigation_layout) as LinearLayout
+        navigation_layout.setBackgroundColor(ContextCompat.getColor(this, R.color.design_default_color_primary))
 
-
-        searchBtn = findViewById(R.id.search_btn) as Button
-        searchBtn?.setOnClickListener {
-            Log.d("TEST::SearchBtnClick", autoCompleteEditText?.text.toString())
-        }
-
-        autoCompleteEditText.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
-
-            val place = parent.getItemAtPosition(position) as PlaceDataModel
-            autoCompleteEditText.apply {
-                Log.d("TEST::autoComplete", place.placeId.toString())
-                Log.d("TEST::autoComplete", position.toString())
-
-                val placeFields: List<Place.Field> = listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS,Place.Field.OPENING_HOURS, Place.Field.WEBSITE_URI, Place.Field.RATING, Place.Field.LAT_LNG)
-                val request = FetchPlaceRequest.newInstance(place.placeId.toString(), placeFields)
-                mPlacesClient.fetchPlace(request)
-                        .addOnSuccessListener { response: FetchPlaceResponse ->
-                        val place = response.place
-                        Log.i("TEST::PlacesClient", "Place Found : ${place.name}")
-                        Log.i("TEST::PlacesClient", "Place Found : ${place.address}")
-                        Log.i("TEST::PlacesClient", "Place Found : ${place.websiteUri}")
-                        Log.i("TEST::PlacesClient", "Place Found : ${place.latLng}")
-                        Log.i("TEST::PlacesClient", "Place Found : ${place.rating}")
-                        Log.i("TEST::PlacesClient", "Place Found : ${place.isOpen}")
-                            setMarkerDetailPlace(place);
-                        }.addOnFailureListener{exception: Exception ->
-                    if (exception is ApiException) {
-                        Log.e("TEST::::PlacesClient", "Place not found: ${exception.message}")
-                        val statusCode = exception.statusCode
-                        TODO("Handle error with given status code")
-                    }
-                }
-                mPosition = position
-                setText(place.fullText)
-                setSelection(autoCompleteEditText.length())
-            }
-        }
     }
 
     private fun updateAdapter(highlightItemPos: Int) {
@@ -245,35 +183,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListen
         super.onPostCreate(savedInstanceState, persistentState)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val JINJU = LatLng((LATLNG["Lat"] ?: error("didn't Double Type")).toDouble(), (LATLNG["Lng"]
-                ?: error("didn't Double Type")).toDouble())
-        val markerOptions = MarkerOptions()
-        markerOptions.position(JINJU)
-        markerOptions.title("JINJU")
-        markerOptions.snippet("GYEUNANM")
-        mMap!!.addMarker(markerOptions)
 
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(JINJU, 10f))
-    }
-
-    override fun onClick(v: View?) {
-
-    }
-    private fun setMarkerDetailPlace(place : Place){
-        if(place.latLng != null){
-            val newPlace = LatLng(place.latLng!!.latitude, place.latLng!!.longitude)
-            val markerOptions = MarkerOptions()
-            markerOptions.position(newPlace)
-            markerOptions.title(place.name.toString())
-            if (place.address != null) markerOptions.snippet(place.address)
-            mMap!!.addMarker(markerOptions)
-            mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(newPlace, 10f))
-        }else {
-            Log.e("TEST", "Place not found!")
-        }
-    }
     private fun getLocationPermission() {
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.INTERNET
